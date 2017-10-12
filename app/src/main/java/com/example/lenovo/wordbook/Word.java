5bfd4dcc9b536848f2b53ba9bfd11086b1be0b17
hym <1349456160@qq.com>
2017-10-12 17:08:01 +0800

    完成了fragment部分的布局

---
+++ app/src/main/java/com/example/lenovo/wordbook/Word.java
@@ -0,0 +1,18 @@
+package com.example.lenovo.wordbook;
+
+public class Word {
+    private String word;
+    private String content;
+    public String getWord(){
+        return word;
+    }
+    public void setWord(String word){
+        this.word=word;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+}
